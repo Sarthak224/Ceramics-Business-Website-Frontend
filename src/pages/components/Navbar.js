8a034c5b48e25aba1e6000191c18971f7c328945
Sarthak224 <madhwalsarthak@gmail.com>
2023-09-24 09:14:16 +0530

    ongoing

--- src/pages/components/Navbar.js
+++ src/pages/components/Navbar.js
@@ -57,7 +57,7 @@ export default function Navbar(){
      
         <div className='nav-comp nav-color nav-middle' >
             <img src={logo} width={80} height={80}  />
-            <span className='brand-text '>React Pottery</span>
+            {/* <span className='brand-text '>React Pottery</span> */}
         </div>
         <div   className='nav-comp nav-color' style={{width:"35%",justifyContent:"end",alignItems:"center",marginRight:"10px",}}>
       
